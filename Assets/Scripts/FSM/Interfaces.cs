ef9f7d8c9f25e8f9a5a1f227e7d1f5b9ee573c06
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2020-04-12 20:28:30 +0700

    Add FSM scripts

---
+++ Assets/Scripts/FSM/Interfaces.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace FSM
+{
+    public interface IContext<TContext>
+        where TContext : IContext<TContext> { }
+
+    public interface IContext : IContext<IContext> { }
+
+    public interface IState<in TContext>
+        where TContext : IContext
+    {
+        IState<TContext> Handle(TContext context);
+    }
+
+    public interface IStateEnter<in TContext>
+        where TContext : IContext
+    {
+        void Enter(TContext context);
+    }
+
+    public interface IStateExit<in TContext>
+        where TContext : IContext
+    {
+        void Exit(TContext context);
+    }
+
+    public interface IAgent<TContext>
+        where TContext : IContext
+    {
+        IState<TContext> CurrentState { get; }
+
+        void SetState(IState<TContext> newState);
+    }
+
+    public interface IStatePluggable<in TContext> : IState<TContext>
+        where TContext : IContext
+    {
+        IEnumerable<IAction<TContext>> Actions { get; }
+    }
+
+    public interface IAction<in TContext>
+        where TContext : IContext
+    {
+        IState<TContext> Apply(TContext context);
+    }
+
+    public interface IActionEnter<in TContext> : IAction<TContext>
+        where TContext : IContext
+    {
+        void Enter(TContext context);
+    }
+
+    public interface IActionExit<in TContext> : IAction<TContext>
+        where TContext : IContext
+    {
+        void Exit(TContext context);
+    }
+}
---
+++ Assets/Scripts/FSM/Scriptables.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace FSM
+{
+    public abstract class MonoContext
+        : MonoBehaviour, IContext { }
+
+    public abstract class ScriptableState<TContext>
+        : ScriptableObject, IState<TContext>
+        where TContext : MonoContext
+    {
+        public abstract IState<TContext> Handle(TContext context);
+    }
+
+    public abstract class MonoAgent<TContext>
+        : MonoBehaviour, IAgent<TContext>
+        where TContext : MonoContext
+    {
+        public abstract TContext Context { get; }
+        public abstract IState<TContext> CurrentState { get; }
+
+        public abstract void SetState(IState<TContext> newState);
+
+        protected void Start()
+        {
+            SetState(CurrentState);
+        }
+
+        protected void Update()
+        {
+            var newState = CurrentState?.Handle(Context);
+            if (newState != null)
+            {
+                SetState(newState);
+            }
+        }
+    }
+
+    public abstract class ScriptableAction<TContext>
+        : ScriptableObject, IAction<TContext>
+        where TContext : MonoContext
+    {
+        public abstract IState<TContext> Apply(TContext context);
+    }
+}
